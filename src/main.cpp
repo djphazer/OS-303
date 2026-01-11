694587a74fbe14c09452c9049efa6778101ed3f8
Nicholas J. Michalek <djphazer@gmail.com>
2026-01-11 10:16:06 -0500

    Big Test for inputs (borked)

--- src/main.cpp
+++ src/main.cpp
@@ -64,13 +64,13 @@ enum PinoutDefs : uint8_t {
 };
 
 // Ports A and B are inputs
-uint8_t INPUTS[] = {
+const uint8_t INPUTS[] = {
   PA0_PIN, PA1_PIN, PA2_PIN, PA3_PIN,
   PB0_PIN, PB1_PIN, PB2_PIN, PB3_PIN,
 };
 // Ports C, D, E, and F are both ways?
 // Ports G and H are outputs
-uint8_t OUTPUTS[] = {
+const uint8_t OUTPUTS[] = {
   PC0_PIN, PC1_PIN, PC2_PIN, PC3_PIN,
   PD0_PIN, PD1_PIN, PD2_PIN, PD3_PIN,
   PE0_PIN, PE1_PIN, PE2_PIN, PE3_PIN,
@@ -80,24 +80,6 @@ uint8_t OUTPUTS[] = {
   PI1_PIN, PI2_PIN,
 };
 
-struct SwitchedPin {
-  uint8_t addr0, addr1, pin;
-};
-
-uint8_t led_data[4] = {
-  PG0_PIN, PG1_PIN, PG2_PIN, PG3_PIN,
-};
-uint8_t select_pin[4] = {
-  PH0_PIN, PH1_PIN, PH2_PIN, PH3_PIN,
-};
-
-uint8_t direct_outs[] = {
-  //PG0_PIN, PG1_PIN, PG2_PIN, PG3_PIN,
-  PC0_PIN, PC1_PIN, PC2_PIN, PC3_PIN,
-  PI1_PIN, PI2_PIN,
-};
-
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 /*
  * I think PH pins are used to select which buttons/LEDs to engage using PG and PB
  * and probably other stuff, too - C# & D# buttons, which status lines to read on PA,
@@ -110,6 +92,78 @@ uint8_t direct_outs[] = {
  *
  * CV Out seems more complex... a D/A converter is fed by the PD & PF pins
  */
+
+//
+// *** Utilities ***
+//
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+
+struct PinPair {
+  uint8_t led, button;
+};
+struct MatrixPin {
+  uint8_t select, led, button;
+};
+
+bool Input(MatrixPin pair) {
+  digitalWriteFast(pair.select, LOW);
+  return digitalRead(pair.button);
+}
+
+void SendCV() {
+  digitalWrite(PI1_PIN, HIGH);
+  digitalWrite(PI1_PIN, LOW);
+}
+
+void SetGate(bool on) {
+  digitalWriteFast(PI2_PIN, on? HIGH : LOW);
+}
+
+//
+// --- useful pin information ---
+//
+const uint8_t select_pin[4] = {
+  PH0_PIN, PH1_PIN, PH2_PIN, PH3_PIN,
+};
+
+const PinPair button_led_pairs[4] = {
+    {PB0_PIN, PG0_PIN},
+    {PB1_PIN, PG1_PIN},
+    {PB2_PIN, PG2_PIN},
+    {PB3_PIN, PG3_PIN},
+};
+
+const uint8_t direct_leds[] = {
+  PC0_PIN, PC1_PIN, PC2_PIN, PC3_PIN,
+};
+
+/*
+  PG0_PIN = 42, // [1], [DEL], [DOWN], [5]
+  PG1_PIN = 43, // [2], [INS], [UP], [6]
+  PG2_PIN = 44, // [3], [F#], [ACCENT], [7]
+  PG3_PIN = 45, // [4], [G#], [SLIDE], [8]
+*/
+const MatrixPin buttonLeds[] = {
+  {PH0_PIN, PG0_PIN, PB0_PIN}, // [1] key, C
+  {PH0_PIN, PG1_PIN, PB1_PIN}, // [2] key, D
+  {PH0_PIN, PG2_PIN, PB2_PIN}, // [3] key, E
+  {PH0_PIN, PG3_PIN, PB3_PIN}, // [4] key, F
+
+  {PH1_PIN, PG0_PIN, PB0_PIN}, // [5] key, G
+  {PH1_PIN, PG1_PIN, PB1_PIN}, // [6] key, A
+  {PH1_PIN, PG2_PIN, PB2_PIN}, // [7] key, B
+  {PH1_PIN, PG3_PIN, PB3_PIN}, // [8] key, C
+
+  {PH2_PIN, PG0_PIN, PB0_PIN}, // [9] or [DOWN]
+  {PH2_PIN, PG1_PIN, PB1_PIN}, // [0] or [UP]
+  {PH2_PIN, PG2_PIN, PB2_PIN}, // [100] or [ACCENT]
+  {PH2_PIN, PG3_PIN, PB3_PIN}, // [200] or [SLIDE]
+
+  {PH3_PIN, PG0_PIN, PB0_PIN}, // [DEL] or C#
+  {PH3_PIN, PG1_PIN, PB1_PIN}, // [INS] or D#
+  {PH3_PIN, PG2_PIN, PB2_PIN}, // F#
+  {PH3_PIN, PG3_PIN, PB3_PIN}, // G#
+};
 
 void setup() {
   Serial.begin(9600);
@@ -124,53 +178,57 @@ void setup() {
   }
 }
 
-void Clock() {
-  digitalWrite(PI1_PIN, HIGH); // Clock
-  delay(1);
-  digitalWrite(PI1_PIN, LOW);
-}
-
 void loop() {
-  for (size_t i = 0; i < ARRAY_SIZE(direct_outs); ++i) {
-    digitalWrite(direct_outs[i], HIGH);
+  static uint32_t ticks = 0;
+  /*
+  for (size_t i = 0; i < ARRAY_SIZE(direct_leds); ++i) {
+    digitalWrite(direct_leds[i], HIGH);
     delay(100);
-    digitalWrite(direct_outs[i], LOW);
+    digitalWrite(direct_leds[i], LOW);
     delay(50);
   }
+  */
 
-  // Accent seems to require PE0 & PI1...
+  // Accent seems to use PE0
+  /*
   digitalWrite(PE0_PIN, LOW);
-  Clock();
+  SendCV();
   delay(100);
   digitalWrite(PE0_PIN, HIGH);
-  Clock();
+  SendCV();
   delay(50);
+  */
 
-  for (uint8_t i = 0; i < 4; ++i) {
-    digitalWrite(select_pin[i], LOW);
-    for (uint8_t j = 0; j < 4; ++j) {
-      digitalWrite(led_data[j], HIGH);
-      delay(100);
-      digitalWrite(led_data[j], LOW);
-      delay(50);
+  // switched LED matrix here
+  // Starting to look like we gotta read and write each matrix cell at the same time
+  for (size_t i = 0; i < ARRAY_SIZE(select_pin); ++i) {
+    bool buttonstate[4] = {false};
+
+    // read buttons
+    for (int j = 0; j < 4; ++j) {
+      buttonstate[j] = digitalRead(button_led_pairs[j].button);
     }
-    digitalWrite(select_pin[i], HIGH);
+
+    digitalWrite(select_pin[i], LOW); // open channel for new values
+
+    // echo the buttons with the LEDs
+    for (int j = 0; j < 4; ++j) {
+      digitalWrite(button_led_pairs[j].led, !buttonstate[j]);
+    }
+
+    digitalWrite(select_pin[i], HIGH); // hold values
   }
 
-  digitalWrite(PD0_PIN, HIGH);
-  digitalWrite(PD1_PIN, HIGH);
-  digitalWrite(PD2_PIN, HIGH);
-  digitalWrite(PD3_PIN, HIGH);
-  digitalWrite(PF0_PIN, HIGH);
-  digitalWrite(PF1_PIN, HIGH);
-  Clock();
-  delay(500);
-  digitalWrite(PD0_PIN, LOW);
-  digitalWrite(PD1_PIN, LOW);
-  digitalWrite(PD2_PIN, LOW);
-  digitalWrite(PD3_PIN, LOW);
-  digitalWrite(PF0_PIN, LOW);
-  digitalWrite(PF1_PIN, LOW);
-  Clock();
-  delay(500);
+  // DAC for CV Out
+  // sending a ramp up as test
+  digitalWrite(PD0_PIN, (ticks >> 0) & 1);
+  digitalWrite(PD1_PIN, (ticks >> 1) & 1);
+  digitalWrite(PD2_PIN, (ticks >> 2) & 1);
+  digitalWrite(PD3_PIN, (ticks >> 3) & 1);
+  digitalWrite(PF0_PIN, (ticks >> 4) & 1);
+  digitalWrite(PF1_PIN, (ticks >> 5) & 1);
+  SendCV();
+
+  ++ticks;
+  delay(1);
 }
