3871ed3255d34e313031aa7a827670c99b293d2a
Nicholas J. Michalek <djphazer@gmail.com>
2026-02-28 17:24:34 -0500

    git commit hash in filename

--- progname.py
+++ progname.py
@@ -1,3 +1,14 @@
 Import("env")
 
-env.Replace(PROGNAME=f"OS-303_v%s" % env.GetProjectOption("project_version"))
+import subprocess
+
+def get_git_rev():
+    git_rev = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode().strip()
+    git_status = subprocess.check_output(["git", "status", "-s", "--untracked-files=no"]).decode()
+    suffix = "dirty" if git_status else ""
+
+    return git_rev + suffix
+
+git_rev = get_git_rev()
+
+env.Replace(PROGNAME=f"OS-303_v%s_{git_rev}" % env.GetProjectOption("project_version"))
